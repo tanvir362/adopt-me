bbe7cde728bebb33f661b156725595d35d6dd753
tanvir362 <tanvir66288@gmai.com>
2020-10-03 18:35:06 +0600

    changed defalt animal to bird

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -7,7 +7,7 @@ const SearchParams = () => {
 
     const [location, setLocation] = useState("Seattle, WA")
     const [breeds, setBreeds] = useState([])
-    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS)
+    const [animal, AnimalDropdown] = useDropdown("Animal", "bird", ANIMALS)
     const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds)
     const [pets, setPets] = useState([])
 
