67eb49d83f8d0dc99c5a6fb09ee227e973e41876
tanvir362 <tanvir66288@gmai.com>
2020-10-03 18:33:38 +0600

    Implemented reach router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 import SearchParams from "./SearchParams"
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" name="Tanvir" />
+      </Router>
     </div>
       
   );
